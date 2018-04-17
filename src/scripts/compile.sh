3ca878c3555810d32c856305197bb58848da88cd
markankaro <markankaro2016@gmail.com>
2018-04-17 18:50:27 +0200

    Remove scripts

--- src/scripts/compile.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-source ./setenv.sh
-javac BarcenasFinder.java BarcenasWorldEnv.java
--- src/scripts/execagents.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-java jade.Boot -agents 'BarcenasWorld:BarcenasWorldEnv(4,3,3);Finder:BarcenasFinder(BarcenasWorld,4,steps.txt)'
